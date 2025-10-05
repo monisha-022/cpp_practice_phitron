4539b500e9739dcb0a62ada1087fee8f420c3013
Monisha farzana <monisha.pompy528@gmail.com>
2025-10-05 21:17:26 +0600

    setprecision.cpp

---
+++ setprecision.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double d = 10.2203;
+    cout << fixed << setprecision(3) << d << endl;
+    return 0;
+}
+
+OUTPUT = 10.220
